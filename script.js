94542afd26b831ea8fe0099960dac1d866d46cfc
ChrisCross19 <49039734+ChrisCross19@users.noreply.github.com>
2021-10-02 16:41:15 +0200

    updated

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function maxLength (){
-    this.value=this.value.replace(/[^0-9]/g,'');
-    console.log("max");
+function maxLength (nr){
+    // this.value=this.value.replace(/[^0-9]/g,'');
+    // console.log("max");
+    switch (nr) {
+        case 2:
+            document.getElementById(digitTwo).focus();
+            break;
+    
+        default:
+            break;
+    }
 }
