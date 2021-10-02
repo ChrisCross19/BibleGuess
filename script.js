237040e41ea11223a21194e0466bdb2431309962
ChrisCross19 <49039734+ChrisCross19@users.noreply.github.com>
2021-10-02 17:18:34 +0200

    updated

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-function maxLength (nr){
+function nextDigit (nr){
     // this.value=this.value.replace(/[^0-9]/g,'');
     // console.log("max");
     switch (nr) {
+        // case 1:
+        //     document.getElementById("digitTwo").focus();
+        //     break;
         case 2:
-            document.getElementById(digitTwo).focus();
+            document.getElementById("digitTwo").focus();
+            break;
+        case 3:
+            document.getElementById("digitThree").focus();
+            break;
+        case 4:
+            document.getElementById("digitFour").focus();
             break;
     
-        default:
+        case 5:
+            console.log("join")
+            fadeOutEffect()
+            document.getElementById("joinSpinner").style = "";
             break;
     }
+}
+
+function fadeOutEffect() {
+    var fadeTarget = document.getElementById("digits");
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 100);
 }
